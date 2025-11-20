4ff482a17dda90b7037664bb92071b8d89271165
Renato Cardoso <renatoscardoso77@hotmail.com>
2025-11-20 14:57:11 -0400

    fix(campaigns): aumentar limite de payload

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import { json, urlencoded } from 'express';
 
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Aumenta o limite de tamanho do corpo das requisições
+  // para suportar imagens base64 em campanhas
+  app.use(json({ limit: '10mb' }));
+  app.use(
+    urlencoded({
+      limit: '10mb',
+      extended: true
+    })
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
